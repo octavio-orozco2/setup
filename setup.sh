a87aea87efd6dad391c2f66b3457a859d10e9fd2
octavio-orozco2 <octavio.orozco2@gmail.com>
2013-08-16 14:49:34 -0500

    Se agrega en la instalación: curl y heroku.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
+sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
@@ -19,6 +20,10 @@ npm install -g jshint
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
